d2faf3ae3f33f5418aee2343f831be9ea1cf02f9
dopa1593 <wkdqkfwkd123456@naver.com>
2022-12-14 11:06:43 +0900

    Post 모델 생성하고 시간 설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    name = models.CharField(max_length=30)
+    nick = models.TextField(null=True, max_length=30)
+
+    p_num = models.TextField(null=True, max_length=30)
+    memo = models.TextField(null=True)
+
+    b_day = models.DateTimeField(null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.name}'
+

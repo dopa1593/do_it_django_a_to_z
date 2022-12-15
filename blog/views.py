666508ae115235b7bae89537fa647c2c860f53a0
dopa1593 <wkdqkfwkd123456@naver.com>
2022-12-15 10:49:27 +0900

    네비게이션바, 연락처페이지 구성

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+
+def index(request):
+    return render(
+        request,
+        'blog/index.html',
+    )
+def posts(request):
+    posts = Post.objects.all()
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'posts': posts,
+        }
+    )
+def info(request):
+    return render(
+        request,
+        'blog/info.html'
+    )
